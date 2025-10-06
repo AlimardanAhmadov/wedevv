e17c43118f11abfa4f4a5adfb851fd2c56b51c50
George Nick <scifundcorp@gmail.com>
2025-10-06 23:00:00 +0800

    removed unnecessary files

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,10 @@
 
 import Footer from "./components/Footer/Footer";
 import Banner from "./components/Home/Banner/Banner";
-import Categories from "./components/Home/Categories/Categories";
-import Coaches from "./components/Home/Coaches/Coaches";
-import CTA from "./components/Home/CTA/CTA";
 import DiscordSection from "./components/Home/DiscordSection/DiscordSection";
 import InfoSection from "./components/Home/InfoSection/InfoSection";
 import Navbar from "./components/Home/Navbar/Navbar";
-import Pricing from "./components/Home/Pricing/Pricing";
-import ProfitCard from "./components/Home/ProfitCard/ProfitCard";
-import Integrations from "./components/Home/Integrations/Integrations";
 import GlobalTrainers from "./components/Home/GlobalTrainers/GlobalTrainers";
-import Globe3D from "./components/Home/3D-Globe/page";
 import MassIconsAnimation from "./components/Home/MassIconsAnimation/page";
 import LoadingSpinner from "./components/Loader/LoadingSpinner";
 import Services from "./components/Home/Services/Services";
