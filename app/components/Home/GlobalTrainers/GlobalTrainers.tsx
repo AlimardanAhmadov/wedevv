eae72236ae6a7305b6ee57db57034326d199f615
George Nick <scifundcorp@gmail.com>
2025-10-17 02:56:01 +0800

    new sections

--- app/components/Home/GlobalTrainers/GlobalTrainers.tsx
+++ app/components/Home/GlobalTrainers/GlobalTrainers.tsx
@@ -98,21 +98,13 @@ export default function GlobalTrainers() {
                 <div className='flex flex-row items-center gap-4 testimonials relative mt-[2rem]'>
                     <div className='rounded-2xl border-2 border-dashed border-[#808080] flex flex-col gap-4 items-start relative p-6 w-max'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="64" height="43" fill="none" viewBox="0 0 84 63" className="absolute top-[-30px] right-0 mr-2 z-1">
-                            <path fill="#36F4A4" fill-rule="evenodd" d="M10.5 0A10.5 10.5 0 000 10.5v15.75a10.5 10.5 0 0010.5 10.5h15.75A15.75 15.75 0 0110.5 52.5H5.25a5.25 5.25 0 100 10.5h5.25a26.25 26.25 0 0026.25-26.25V10.5A10.5 10.5 0 0026.25 0H10.5zm47.25 0a10.5 10.5 0 00-10.5 10.5v15.75a10.5 10.5 0 0010.5 10.5H73.5A15.75 15.75 0 0157.75 52.5H52.5a5.25 5.25 0 100 10.5h5.25A26.25 26.25 0 0084 36.75V10.5A10.5 10.5 0 0073.5 0H57.75z" clip-rule="evenodd"></path>
+                            <path fill="#d96570" fill-rule="evenodd" d="M10.5 0A10.5 10.5 0 000 10.5v15.75a10.5 10.5 0 0010.5 10.5h15.75A15.75 15.75 0 0110.5 52.5H5.25a5.25 5.25 0 100 10.5h5.25a26.25 26.25 0 0026.25-26.25V10.5A10.5 10.5 0 0026.25 0H10.5zm47.25 0a10.5 10.5 0 00-10.5 10.5v15.75a10.5 10.5 0 0010.5 10.5H73.5A15.75 15.75 0 0157.75 52.5H52.5a5.25 5.25 0 100 10.5h5.25A26.25 26.25 0 0084 36.75V10.5A10.5 10.5 0 0073.5 0H57.75z" clip-rule="evenodd"></path>
                             <path fill="url(#paint0_linear_119_6)" fill-rule="evenodd" d="M10.5 0A10.5 10.5 0 000 10.5v15.75a10.5 10.5 0 0010.5 10.5h15.75A15.75 15.75 0 0110.5 52.5H5.25a5.25 5.25 0 100 10.5h5.25a26.25 26.25 0 0026.25-26.25V10.5A10.5 10.5 0 0026.25 0H10.5zm47.25 0a10.5 10.5 0 00-10.5 10.5v15.75a10.5 10.5 0 0010.5 10.5H73.5A15.75 15.75 0 0157.75 52.5H52.5a5.25 5.25 0 100 10.5h5.25A26.25 26.25 0 0084 36.75V10.5A10.5 10.5 0 0073.5 0H57.75z" clip-rule="evenodd"></path>
                             <defs>
                                 <linearGradient id="paint0_linear_119_6" x1="21" x2="84" y1="63" y2="-25.5" gradientUnits="userSpaceOnUse">
-                                    <stop stop-color="#225480ff"></stop><stop offset="1" stop-color="#36F4A4"></stop>
+                                    <stop stop-color="#d96570"></stop><stop offset="1" stop-color="#3983ff"></stop>
                                 </linearGradient>
                             </defs>
-                        </svg>
-                        <svg aria-hidden="true" style={{ fill: '#00000087' }} className="w-full h-full absolute top-0 bottom-0 left-0 right-0 -z-10 rounded-2xl">
-                            <defs>
-                                <pattern id="«R1cdb»" width="10" height="10" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse" x="0" y="0">
-                                <circle id="pattern-circle" cx="1" cy="1" r="1"></circle>
-                                </pattern>
-                            </defs>
-                            <rect width="100%" height="100%" strokeWidth="0" fill="url(#«R1cdb»)"></rect>
                         </svg>
                         <div className='flex flex-col items-start text-[30px] font-semibold'>
                             Cathy Tattoo
@@ -125,21 +117,13 @@ export default function GlobalTrainers() {
                     </div>
                     <div className='rounded-2xl border-2 border-dashed border-[#808080] flex flex-col gap-4 items-start relative p-6 w-max'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="64" height="43" fill="none" viewBox="0 0 84 63" className="absolute top-[-30px] right-0 mr-2 z-1">
-                            <path fill="#36F4A4" fill-rule="evenodd" d="M10.5 0A10.5 10.5 0 000 10.5v15.75a10.5 10.5 0 0010.5 10.5h15.75A15.75 15.75 0 0110.5 52.5H5.25a5.25 5.25 0 100 10.5h5.25a26.25 26.25 0 0026.25-26.25V10.5A10.5 10.5 0 0026.25 0H10.5zm47.25 0a10.5 10.5 0 00-10.5 10.5v15.75a10.5 10.5 0 0010.5 10.5H73.5A15.75 15.75 0 0157.75 52.5H52.5a5.25 5.25 0 100 10.5h5.25A26.25 26.25 0 0084 36.75V10.5A10.5 10.5 0 0073.5 0H57.75z" clip-rule="evenodd"></path>
+                            <path fill="#d96570" fill-rule="evenodd" d="M10.5 0A10.5 10.5 0 000 10.5v15.75a10.5 10.5 0 0010.5 10.5h15.75A15.75 15.75 0 0110.5 52.5H5.25a5.25 5.25 0 100 10.5h5.25a26.25 26.25 0 0026.25-26.25V10.5A10.5 10.5 0 0026.25 0H10.5zm47.25 0a10.5 10.5 0 00-10.5 10.5v15.75a10.5 10.5 0 0010.5 10.5H73.5A15.75 15.75 0 0157.75 52.5H52.5a5.25 5.25 0 100 10.5h5.25A26.25 26.25 0 0084 36.75V10.5A10.5 10.5 0 0073.5 0H57.75z" clip-rule="evenodd"></path>
                             <path fill="url(#paint0_linear_119_6)" fill-rule="evenodd" d="M10.5 0A10.5 10.5 0 000 10.5v15.75a10.5 10.5 0 0010.5 10.5h15.75A15.75 15.75 0 0110.5 52.5H5.25a5.25 5.25 0 100 10.5h5.25a26.25 26.25 0 0026.25-26.25V10.5A10.5 10.5 0 0026.25 0H10.5zm47.25 0a10.5 10.5 0 00-10.5 10.5v15.75a10.5 10.5 0 0010.5 10.5H73.5A15.75 15.75 0 0157.75 52.5H52.5a5.25 5.25 0 100 10.5h5.25A26.25 26.25 0 0084 36.75V10.5A10.5 10.5 0 0073.5 0H57.75z" clip-rule="evenodd"></path>
                             <defs>
                                 <linearGradient id="paint0_linear_119_6" x1="21" x2="84" y1="63" y2="-25.5" gradientUnits="userSpaceOnUse">
-                                    <stop stop-color="#225480ff"></stop><stop offset="1" stop-color="#36F4A4"></stop>
+                                    <stop stop-color="#d96570"></stop><stop offset="1" stop-color="#3983ff"></stop>
                                 </linearGradient>
                             </defs>
-                        </svg>
-                        <svg aria-hidden="true" style={{ fill: '#00000087' }} className="w-full h-full absolute top-0 bottom-0 left-0 right-0 -z-10 rounded-2xl">
-                            <defs>
-                                <pattern id="«R1cdb»" width="10" height="10" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse" x="0" y="0">
-                                <circle id="pattern-circle" cx="1" cy="1" r="1"></circle>
-                                </pattern>
-                            </defs>
-                            <rect width="100%" height="100%" strokeWidth="0" fill="url(#«R1cdb»)"></rect>
                         </svg>
                         <div className='flex flex-col items-start text-[30px] font-semibold'>
                             IV Solutions
@@ -152,21 +136,13 @@ export default function GlobalTrainers() {
                     </div>
                     <div className='rounded-2xl border-2 border-dashed border-[#808080] flex flex-col gap-4 items-start relative p-6 w-max'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="64" height="43" fill="none" viewBox="0 0 84 63" className="absolute top-[-30px] right-0 mr-2 z-1">
-                            <path fill="#36F4A4" fill-rule="evenodd" d="M10.5 0A10.5 10.5 0 000 10.5v15.75a10.5 10.5 0 0010.5 10.5h15.75A15.75 15.75 0 0110.5 52.5H5.25a5.25 5.25 0 100 10.5h5.25a26.25 26.25 0 0026.25-26.25V10.5A10.5 10.5 0 0026.25 0H10.5zm47.25 0a10.5 10.5 0 00-10.5 10.5v15.75a10.5 10.5 0 0010.5 10.5H73.5A15.75 15.75 0 0157.75 52.5H52.5a5.25 5.25 0 100 10.5h5.25A26.25 26.25 0 0084 36.75V10.5A10.5 10.5 0 0073.5 0H57.75z" clip-rule="evenodd"></path>
+                            <path fill="#d96570" fill-rule="evenodd" d="M10.5 0A10.5 10.5 0 000 10.5v15.75a10.5 10.5 0 0010.5 10.5h15.75A15.75 15.75 0 0110.5 52.5H5.25a5.25 5.25 0 100 10.5h5.25a26.25 26.25 0 0026.25-26.25V10.5A10.5 10.5 0 0026.25 0H10.5zm47.25 0a10.5 10.5 0 00-10.5 10.5v15.75a10.5 10.5 0 0010.5 10.5H73.5A15.75 15.75 0 0157.75 52.5H52.5a5.25 5.25 0 100 10.5h5.25A26.25 26.25 0 0084 36.75V10.5A10.5 10.5 0 0073.5 0H57.75z" clip-rule="evenodd"></path>
                             <path fill="url(#paint0_linear_119_6)" fill-rule="evenodd" d="M10.5 0A10.5 10.5 0 000 10.5v15.75a10.5 10.5 0 0010.5 10.5h15.75A15.75 15.75 0 0110.5 52.5H5.25a5.25 5.25 0 100 10.5h5.25a26.25 26.25 0 0026.25-26.25V10.5A10.5 10.5 0 0026.25 0H10.5zm47.25 0a10.5 10.5 0 00-10.5 10.5v15.75a10.5 10.5 0 0010.5 10.5H73.5A15.75 15.75 0 0157.75 52.5H52.5a5.25 5.25 0 100 10.5h5.25A26.25 26.25 0 0084 36.75V10.5A10.5 10.5 0 0073.5 0H57.75z" clip-rule="evenodd"></path>
                             <defs>
                                 <linearGradient id="paint0_linear_119_6" x1="21" x2="84" y1="63" y2="-25.5" gradientUnits="userSpaceOnUse">
-                                    <stop stop-color="#225480ff"></stop><stop offset="1" stop-color="#36F4A4"></stop>
+                                    <stop stop-color="#d96570"></stop><stop offset="1" stop-color="#3983ff"></stop>
                                 </linearGradient>
                             </defs>
-                        </svg>
-                        <svg aria-hidden="true" style={{ fill: '#00000087' }} className="w-full h-full absolute top-0 bottom-0 left-0 right-0 -z-10 rounded-2xl">
-                            <defs>
-                                <pattern id="«R1cdb»" width="10" height="10" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse" x="0" y="0">
-                                <circle id="pattern-circle" cx="1" cy="1" r="1"></circle>
-                                </pattern>
-                            </defs>
-                            <rect width="100%" height="100%" strokeWidth="0" fill="url(#«R1cdb»)"></rect>
                         </svg>
                         <div className='flex flex-col items-start text-[30px] font-semibold'>
                             Agatos
